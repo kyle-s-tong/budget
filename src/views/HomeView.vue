70931361c9f1ea6e3750b1d235f77fb5f1fa9ff1
Kyle Tong <kyle.simon.tong@gmail.com>
2022-05-19 17:34:38 +0900

    Add total balance section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,14 +22,17 @@ export default {
 <template>
   <main class="h-full w-full flex flex-col items-center justify-center">
     <div class="h-3/4 w-full flex">
-      <div class="w-2/3 grid grid-cols-3 gap-4 px-2">
-        <div
-          v-for="envelope in envelopes"
-          :key="envelope.name"
-          class="flex flex-col border border-1"
-        >
-          {{ envelope.name }}
-          {{ envelope.balance }}
+      <div class="flex flex-col w-2/3 px-2">
+        <div class="h-1/4">Current balance: 12304124.231</div>
+        <div class="h-3/4 grid grid-cols-3 gap-2">
+          <div
+            v-for="envelope in envelopes"
+            :key="envelope.name"
+            class="flex flex-col border border-1"
+          >
+            {{ envelope.name }}
+            {{ envelope.balance }}
+          </div>
         </div>
       </div>
       <div class="w-1/3">test</div>
